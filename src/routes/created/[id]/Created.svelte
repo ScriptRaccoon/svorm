2d48709dde167434495ac0a6adf6f990ae027fb8
Script Raccoon <scriptraccoon@gmail.com>
2023-02-26 21:07:00 +0100

    another renaming

--- src/routes/created/[id]/Created.svelte
+++ src/routes/created/[id]/Created.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { page } from "$app/stores";
 	const id = $page.params.id;
-	const url = `${$page.url.origin}/submit/${id}`;
+	const url = `${$page.url.origin}/submission/${id}`;
 </script>
 
 <h2>Svorm has been created</h2>
