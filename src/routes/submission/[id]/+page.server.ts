923824d688e3544e4995443d289f3a752dcbbe12
Script Raccoon <scriptraccoon@gmail.com>
2023-02-26 21:42:11 +0100

    change example svorm

--- src/routes/submission/[id]/+page.server.ts
+++ src/routes/submission/[id]/+page.server.ts
@@ -2,18 +2,24 @@ import type { PageServerLoad } from "./$types";
 
 const exampleSvorm: svorm = {
 	id: "asdasjkhdjwe",
-	title: "example svorm",
+	title: "Weekday survey",
 	elements: [
 		{
 			required: true,
-			id: "35892eb5-74f4-4406-bdef-e27caebccfa3",
-			question: "how did you find here?"
+			id: "59a01248-bb5b-4d45-8081-ee17f0e0511d",
+			question: "What is your employment status?",
+			choices: ["Student", "Worker", "Employee", "Self-employed", "Director", "None"]
+		},
+		{
+			required: true,
+			id: "d767abac-496a-481e-964e-1d0fd5ad4d5e",
+			question: "What is your favorite day of the week?",
+			choices: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 		},
 		{
 			required: false,
-			id: "d767abac-496a-481e-964e-1d0fd5ad4d5e",
-			question: "which is your favorite day?",
-			choices: ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+			id: "35892eb5-74f4-4406-bdef-e27caebccfa3",
+			question: "Why is that your favorite day?"
 		}
 	]
 };
