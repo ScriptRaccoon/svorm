df26830cf12b1d354098d07af69abbe237a257c8
Script Raccoon <scriptraccoon@gmail.com>
2023-03-01 09:37:09 +0100

    improve error handling

--- src/db/element.ts
+++ src/db/element.ts
@@ -22,28 +22,41 @@ export async function save_elements(
 	await supabase.from("multiple_choice").insert(multiple_choices);
 }
 
-export async function get_questions(svorm_id: string) {
+export async function get_questions(
+	svorm_id: string
+): Promise<question_db[] | null> {
 	const { data, error } = await supabase
 		.from("question")
 		.select()
 		.eq("svorm_id", svorm_id);
-	return { data, error };
+	if (error || !data) {
+		console.log(error);
+		return null;
+	}
+	return data as question_db[];
 }
 
-export async function get_multiple_choices(svorm_id: string) {
+export async function get_multiple_choices(
+	svorm_id: string
+): Promise<multiple_choice_db[] | null> {
 	const { data, error } = await supabase
 		.from("multiple_choice")
 		.select()
 		.eq("svorm_id", svorm_id);
-	return { data, error };
+	if (error || !data) {
+		console.log(error);
+		return null;
+	}
+	return data as multiple_choice_db[];
 }
 
-export async function get_elements(svorm_id: string) {
-	const questions = (await get_questions(svorm_id))
-		.data as question_db[];
-	const multiple_choices = (await get_multiple_choices(svorm_id))
-		.data as multiple_choice_db[];
-	const elements: element_db[] = [...questions, ...multiple_choices];
+export async function get_elements(
+	svorm_id: string
+): Promise<element_db[] | null> {
+	const questions = await get_questions(svorm_id);
+	const multiple_choices = await get_multiple_choices(svorm_id);
+	if (!questions || !multiple_choices) return null;
+	const elements: element_db[] = questions.concat(multiple_choices);
 	const sorted_elements = elements.sort((e, f) => e.index - f.index);
 	return sorted_elements;
 }
