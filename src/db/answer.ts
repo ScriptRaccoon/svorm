fe9c524a085fd4030b1846ebd1d4e7e72e04f7f9
Script Raccoon <scriptraccoon@gmail.com>
2023-03-02 00:20:59 +0100

    dont send empty responses

--- src/db/answer.ts
+++ src/db/answer.ts
@@ -3,12 +3,12 @@ import { supabase } from "./supabase";
 export async function save_submission(
 	submission: submission
 ): Promise<boolean> {
-	const saved_questions = Object.entries(
-		submission.answers_questions
-	).map(([question_id, answer]) => ({
-		question_id,
-		answer
-	}));
+	const saved_questions = Object.entries(submission.answers_questions)
+		.map(([question_id, answer]) => ({
+			question_id,
+			answer
+		}))
+		.filter((q) => q.answer.length > 0);
 
 	const { error: question_error } = await supabase
 		.from("questions_answers")
@@ -20,10 +20,12 @@ export async function save_submission(
 
 	const saved_multiple_choices = Object.entries(
 		submission.answers_multiple_choices
-	).map(([multiple_choice_id, answer]) => ({
-		multiple_choice_id,
-		answer
-	}));
+	)
+		.map(([multiple_choice_id, answer]) => ({
+			multiple_choice_id,
+			answer
+		}))
+		.filter((m) => m.answer !== null);
 
 	const { error: multiple_choice_error } = await supabase
 		.from("multiple_choices_answers")
