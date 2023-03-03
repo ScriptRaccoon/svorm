c26983185134d3244284a1fbf1c35b5f96529742
Script Raccoon <scriptraccoon@gmail.com>
2023-03-03 19:05:49 +0100

    save answers to separate tables

--- src/db/answer.ts
+++ src/db/answer.ts
@@ -3,10 +3,33 @@ import { supabase } from "./supabase";
 export async function save_answers(
 	answers: answer[]
 ): Promise<boolean> {
-	const { error } = await supabase.from("answers").insert(answers);
+	const simple_questions_answers = [];
+	const multiple_choices_answers = [];
 
-	if (error) {
-		console.log({ error });
+	for (let i = 0; i < answers.length; i++) {
+		const answer = answers[i];
+		if ("answer" in answer) {
+			simple_questions_answers.push(answer);
+		} else {
+			multiple_choices_answers.push(answer);
+		}
+	}
+
+	const { error: error1 } = await supabase
+		.from("simple_questions_answers")
+		.insert(simple_questions_answers);
+
+	if (error1) {
+		console.log({ error: error1 });
+		return false;
+	}
+
+	const { error: error2 } = await supabase
+		.from("multiple_choices_answers")
+		.insert(multiple_choices_answers);
+
+	if (error2) {
+		console.log({ error: error1 });
 		return false;
 	}
 
