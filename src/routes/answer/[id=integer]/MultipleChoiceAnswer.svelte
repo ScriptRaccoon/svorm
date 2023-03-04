3c3e360c5c55db25ed4ada06aa16bf486e949911
Script Raccoon <scriptraccoon@gmail.com>
2023-03-04 15:59:47 +0100

    reset choice function

--- src/routes/answer/[id=integer]/MultipleChoiceAnswer.svelte
+++ src/routes/answer/[id=integer]/MultipleChoiceAnswer.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	export let question: multiple_choice_db;
 	export let choices_object: Record<string, number> = {};
+
+	function reset_choice() {
+		delete choices_object[question.id];
+		choices_object = choices_object;
+	}
 </script>
 
 <div class="choices">
@@ -15,6 +20,12 @@
 		</label>
 	{/each}
 </div>
+
+<menu>
+	<button type="button" on:click={reset_choice} class="small">
+		Reset
+	</button>
+</menu>
 
 <style>
 	.choices {
